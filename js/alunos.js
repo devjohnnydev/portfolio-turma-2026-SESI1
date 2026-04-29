967213d316c8d040247cbfbc6a4181dd0612d5eb
devjohnnydev <johnnybraga2@gmail.com>
2026-04-29 09:25:12 -0300

    atualização do projeto

--- js/alunos.js
+++ js/alunos.js
@@ -1,134 +1,34 @@
 const alunos = [
-  {
-    nome: "Aron Kazuki Osako Maestá",
-    pasta: "aron-maesta"
-  },
-  {
-    nome: "Arthur Galvão Martins",
-    pasta: "arthur-martins"
-  },
-  {
-    nome: "Daniel Vara dos Santos Ribeiro",
-    pasta: "daniel-ribeiro"
-  },
-  {
-    nome: "Davi Muniz da Silva",
-    pasta: "davi-silva"
-  },
-  {
-    nome: "Diogo Ferrata Marques de Freitas",
-    pasta: "diogo-freitas"
-  },
-  {
-    nome: "Eduardo Vieira Roque",
-    pasta: "eduardo-roque"
-  },
-  {
-    nome: "Emily Ribeiro Lourenço",
-    pasta: "emily-lourenco"
-  },
-  {
-    nome: "Felippe Takahashi Gomes",
-    pasta: "felippe-gomes"
-  },
-  {
-    nome: "Giorgio Figueredo de Sousa",
-    pasta: "giorgio-sousa"
-  },
-  {
-    nome: "Guilherme Coutinho Simon",
-    pasta: "guilherme-simon"
-  },
-  {
-    nome: "Guilherme Jorge de Araujo",
-    pasta: "guilherme-araujo"
-  },
-  {
-    nome: "Guilherme Lima Fonseca",
-    pasta: "guilherme-fonseca"
-  },
-  {
-    nome: "Guilherme Marques do Nascimento",
-    pasta: "guilherme-nascimento"
-  },
-  {
-    nome: "Guilherme Vilares Figueiredo Leitão",
-    pasta: "guilherme-leitao"
-  },
-  {
-    nome: "Gustavo Costa Trindade",
-    pasta: "gustavo-trindade"
-  },
-  {
-    nome: "Gustavo Machado Brito",
-    pasta: "gustavo-brito"
-  },
-  {
-    nome: "Heloisa Sabino Bolognesi",
-    pasta: "heloisa-bolognesi"
-  },
-  {
-    nome: "Isabelly do Nascimento Pereira",
-    pasta: "isabelly-pereira"
-  },
-  {
-    nome: "João Vitor Rodrigues Nogueira",
-    pasta: "joao-nogueira"
-  },
-  {
-    nome: "Kaike Brito Moreira",
-    pasta: "kaike-moreira"
-  },
-  {
-    nome: "Kaluanã Leme Vasconcellos Liu",
-    pasta: "kaluana-liu"
-  },
-  {
-    nome: "Lucas Lopes Varela",
-    pasta: "lucas-varela"
-  },
-  {
-    nome: "Luiz Eduardo Davi",
-    pasta: "luiz-davi"
-  },
-  {
-    nome: "Maria Eduarda Andrade Ferreira",
-    pasta: "maria-ferreira"
-  },
-  {
-    nome: "Maria Laura Carezzato de Lima Pestana",
-    pasta: "maria-pestana"
-  },
-  {
-    nome: "Murillo Alexandre da Silva",
-    pasta: "murillo-silva"
-  },
-  {
-    nome: "Nicolas Rodrigues da Silva Gomes",
-    pasta: "nicolas-gomes"
-  },
-  {
-    nome: "Noah Ben Ariel Haddad Ferreira",
-    pasta: "noah-ferreira"
-  },
-  {
-    nome: "Pedro Garcia Batista",
-    pasta: "pedro-batista"
-  },
-  {
-    nome: "Pedro Matos da Silva",
-    pasta: "pedro-silva"
-  },
-  {
-    nome: "Rafaela Nunes de Souza",
-    pasta: "rafaela-souza"
-  },
-  {
-    nome: "Raul Brandão de Oliveira",
-    pasta: "raul-oliveira"
-  },
-  {
-    nome: "Yan Barbosa da Silva",
-    pasta: "yan-silva"
-  }
+  { nome: "Alexandre de Lima Junior", pasta: "alexandre-junior" },
+  { nome: "Alexandre Henrique Pereira de Araujo", pasta: "alexandre-araujo" },
+  { nome: "Andrey Canalli Rodrigues", pasta: "andrey-rodrigues" },
+  { nome: "Breno Santos de Oliveira", pasta: "breno-oliveira" },
+  { nome: "Cleiton Candido da Silva Junior", pasta: "cleiton-junior" },
+  { nome: "Danilo Gomes Silva", pasta: "danilo-gomes" },
+  { nome: "Danilo Gonçalves da Silva", pasta: "danilo-goncalves" },
+  { nome: "Davi Moreira", pasta: "davi-moreira" },
+  { nome: "Davy Santos Gama", pasta: "davy-gama" },
+  { nome: "Eduardo Dias Roxo da Silva", pasta: "eduardo-roxo" },
+  { nome: "Geovana Reis de Souza", pasta: "geovana-souza" },
+  { nome: "Giovanna Ramos Valério", pasta: "giovanna-valerio" },
+  { nome: "Giovanni Sacco Prando", pasta: "giovanni-prando" },
+  { nome: "Guilherme Miranda Murillo", pasta: "guilherme-murillo" },
+  { nome: "Guilherme Santos Araujo", pasta: "guilherme-araujo" },
+  { nome: "Gustavo Oliveira Almeida", pasta: "gustavo-almeida" },
+  { nome: "Gustavo Viveiros de Brito", pasta: "gustavo-brito" },
+  { nome: "Higor Torres Barbosa Cigo", pasta: "higor-cigo" },
+  { nome: "Igor Bergas Rodrigues da Costa", pasta: "igor-costa" },
+  { nome: "Italo Pereira Gomes", pasta: "italo-gomes" },
+  { nome: "João Victhor Santos de Oliveira", pasta: "joao-victhor" },
+  { nome: "Lara Pecellin Sato", pasta: "lara-sato" },
+  { nome: "Leonardo Ferreira de Paiva", pasta: "leonardo-paiva" },
+  { nome: "Lucas Favalli Barbosa da Silva", pasta: "lucas-favalli" },
+  { nome: "Luiza Cabral Crepaldi", pasta: "luiza-crepaldi" },
+  { nome: "Maria Eduarda Bucci Costa", pasta: "maria-bucci" },
+  { nome: "Nicolas da Silva Mira", pasta: "nicolas-mira" },
+  { nome: "Pedro Otavio Martins Laube", pasta: "pedro-laube" },
+  { nome: "Rafael Rodrigues Pimentel", pasta: "rafael-pimentel" },
+  { nome: "Renan Alves Ferreira Silva", pasta: "renan-ferreira" },
+  { nome: "Rui Kojima Bastos", pasta: "rui-bastos" },
+  { nome: "Thaís Poma da Silva", pasta: "thais-silva" }
 ];
